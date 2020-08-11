78a78933c211d9cf0a0f5e16f276379108539a9c
praveen <praveen@webenza.com>
2020-08-12 00:57:49 +0530

    [2020-08-12 00:57:49] updated the changes

--- application/views/main/project_detail.php
+++ application/views/main/project_detail.php
@@ -1,34 +1,135 @@
 <main>
-        <section class="container-fluid" id="projects-breadcrumb">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <nav aria-label="breadcrumb">
-                            <ol class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item"><a href="#">Projects</a></li>
-                                <li class="breadcrumb-item active" aria-current="page"><a href="">MILROC KADAMBA</a>
-                                </li>
-                            </ol>
-                        </nav>
-                    </div>
+    <section class="container-fluid" id="projects-breadcrumb">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Projects</a></li>
+                            <li class="breadcrumb-item active" aria-current="page"><a href="">MILROC KADAMBA</a>
+                            </li>
+                        </ol>
+                    </nav>
                 </div>
             </div>
-        </section>
+        </div>
+    </section>
 
 
-        <section class="container-fluid" id="project-details-two">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-4 col-sm-12 col-lg-4 col-12 side--nav">
-                        <ul>
-                            <li class="active"><span></span><a href="">OVERVIEW</a></li>
-                            <li><span></span><a href="">PROJECT FEATURES</a></li>
-                            <li><span></span><a href="">SITE PLAN</a></li>
-                            <li><span></span><a href="">FLOOR PLAN</a></li>
+    <section class="container-fluid" id="project-details-two">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-12 col-lg-3 col-12 side--nav">
+                    <ul>
+                        <li class="active"><span></span><a href="">OVERVIEW</a></li>
+                        <li><span></span><a href="">PROJECT FEATURES</a></li>
+                        <li><span></span><a href="">SITE PLAN</a></li>
+                        <li><span></span><a href="">FLOOR PLAN</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-9 col-lg-9 col-sm-12 col-12 content-navigation">
+                    <div class="section-wrap" id="project-deatail-one">
+                        <div class="video-sec relative">
+                            <img src="images/kadamba.png" class="img-fluid w-100">
+                            <div class="play--btn modal-videos"
+                                data-vsrc="https://s2.q4cdn.com/482484005/files/videos/EOP_REIT_August-2019_BestVersion.mp4">
+                                <span>VIDEO</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="section-wrap" id="project-deatail-two">
+                        <div class="row justify-content-between">
+                            <div class="custom-project-col">
+                                <h3><span>MILROC</span> KADAMBA</h3>
+                                <p>Old Goa road, Goa Velha</p>
+                                <p>Residential | 2 and 3 BHK apartments</p>
+                                <span class="flats">438 Flats</span>
+                                <span class="shops">36 Shops</span>
+                            </div>
+                            <div class="custom-project-col">
+                                <a href="#">
+                                    <img src="./images/kadamba_logo.png" class="img-fluid">
+                                    <span>GO TO SITE</span>
+                                </a>
+                            </div>
+                            <p>Welcome to Milroc Kadamba, a premium residential apartment community. Designed with
+                                understated luxury in mind, its
+                                spacious homes with contemporary facilities merge eco-friendly living with the
+                                finesse of a modern lifestyle.</p>
+                        </div>
+
+                    </div>
+
+                    <div class="section-wrap">
+                        <hr>
+                    </div>
+
+                    <div class="section-wrap" id="project-detail-three">
+                        <h4>PROJECT FEATURES</h4>
+                        <ul class="nav">
+                            <li style="background: url(/images/air.png);">Spacious, well ventilated apartments</li>
+                            <li style="background: url(/images/plant.png);">Peaceful and pleasant surroundings</li>
+                            <li style="background: url(/images/garden.png);">Landscaped gardens and recreational
+                                spaces
+                            </li>
+                            <li style="background: url(/images/flower.png);">Hotel and Spa</li>
+                            <li style="background: url(/images/rain.png);">Rainwater harvesting</li>
+                            <li style="background: url(/images/treatment.png);">Garbage disposal plant
+                                (Vermicomposting)
+                            </li>
+                            <li style="background: url(/images/power.png);">Solar water heating panels</li>
+                            <li style="background: url(/images/vectorobject.png);">Proximity to daily necessities
+                            </li>
                         </ul>
                     </div>
+
+                    <div class="section-wrap">
+                        <hr>
+                    </div>
+
+                    <div class="section-wrap" id="project-detail-four">
+                        <h4>SITE PLAN</h4>
+                        <img src="images/kadamba-plan.png" class="img-fluid" alt="">
+                    </div>
+
+                    <div class="section-wrap">
+                        <hr>
+                    </div>
+
+                    <div class="section-wrap" id="project-detail-five">
+                        <h4>LOCATION MAP</h4>
+                        <p>Located 1 km from world famous heritage site of Basilica of Bom Jesus and SE Cathedral &
+                            famous archaeological museum in
+                            Old Goa. On Old Goa Panjim Bypass Road, this place called the Kadamba Plateau.</p>
+                        <a href="#" class="see-map">GOOGLE MAP</a>
+                    </div>
+
+                    <div class="section-wrap">
+                        <hr>
+                    </div>
+
+                    <div class="section-wrap" id="project-detail-six">
+                        <h4>FLOOR PLAN</h4>
+                        <div class="row justify-content-between">
+                            <div class="site-pan">
+                                <img src="images/kadamba_plan_a.png" class="img-fluid w-100" alt="">
+                            </div>
+
+                            <div class="site-pan">
+                                <img src="images/kadamba_plan_d.png" class="img-fluid w-100" alt="">
+                            </div>
+                            <div class="site-pan">
+                                <img src="images/kadamba_plan_h.png" class="img-fluid w-100" alt="">
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
             </div>
-        </section>
-    </main>
+        </div>
+    </section>
+</main>
